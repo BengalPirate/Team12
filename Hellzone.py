fc7c95525ba57078eaf16040b85aef7b1c019398
BengalPirate <J00610524@students.jsums.edu>
2023-06-04 00:06:22 -0400

    bullet mechanics

--- Hellzone.py
+++ Hellzone.py
@@ -1,74 +1,118 @@
-#Importing necessary modules
 import pygame
 import sys 
+import math
 
-#initializing all imported pygame modules
 pygame.init()
 
-# creates a window for diaplaying the game
 display = pygame.display.set_mode((800, 600))
-
-# creates a clock object that can be used to track time
 clock = pygame.time.Clock()
 
-# Defines a class Player
 class Player:
-    # The init method is the intializer (similar to a constructor) that gets called
-    # when a new object is created from the class
     def __init__(self, x, y, width, height):
-        # x and y are the coordinates of the player
         self.x = x
         self.y = y
-        # width and height are the dimensions of the player
         self.width = width
         self.height = height
-    # Defining the main mething which is responsible for drawing the player object
-    def main(self, display):
-        pygame.draw.rect(display, (0,0,0), (self.x, self.y, self.width, self.height))
+        self.speed = 5
 
-# creating a player bject at position (400,300) with width and height of (32, 32)
+    def main(self, display, scroll):
+        pygame.draw.rect(display, (0,0,0), (self.x - scroll[0], self.y - scroll[1], self.width, self.height))
+
+class PlayerBullet: 
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.speed = 10
+        self.direction = direction
+
+        if direction == "up":
+            self.x_vel = 0
+            self.y_vel = -self.speed
+        elif direction == "down":
+            self.x_vel = 0
+            self.y_vel = self.speed
+        elif direction == "left":
+            self.x_vel = -self.speed
+            self.y_vel = 0
+        elif direction == "right":
+            self.x_vel = self.speed
+            self.y_vel = 0
+        elif direction == "northeast":
+            self.x_vel = self.speed / math.sqrt(2)
+            self.y_vel = -self.speed / math.sqrt(2)
+        elif direction == "northwest":
+            self.x_vel = -self.speed / math.sqrt(2)
+            self.y_vel = -self.speed / math.sqrt(2)
+        elif direction == "southeast":
+            self.x_vel = self.speed / math.sqrt(2)
+            self.y_vel = self.speed / math.sqrt(2)
+        elif direction == "southwest":
+            self.x_vel = -self.speed / math.sqrt(2)
+            self.y_vel = self.speed / math.sqrt(2)
+
+    def main(self, display, scroll):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+        pygame.draw.circle(display, (0,0,0), (self.x - scroll[0], self.y - scroll[1]), 5)
+
 player = Player(400, 300, 32, 32) 
-
-#initializing a scroll variable for the display
 display_scroll = [0,0]
+player_bullets = []
 
-#starting the main game loop
 while True:
-
-    #filling the display with red color
     display.fill((255,0,0))
 
-    #getting all the events from the queue
     for event in pygame.event.get():
-        #if the event is quit (closing the window), terminate the program
         if event.type == pygame.QUIT:
-            sys.exit(
-            pygame.QUIT
-            )
-    # getting the state of all keyboard buttons
-    keys = pygame.key.get_pressed()
+            sys.exit()
 
-    #drawing a white rectangle on the display at position (100,100) with width and height of 16 pixels
+    keys = pygame.key.get_pressed()
+    bullet_fired = False
+
+    if keys[pygame.K_UP] and keys[pygame.K_RIGHT] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "northeast"))
+        bullet_fired = True
+    elif keys[pygame.K_UP] and keys[pygame.K_LEFT] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "northwest"))
+        bullet_fired = True
+    elif keys[pygame.K_DOWN] and keys[pygame.K_RIGHT] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "southeast"))
+        bullet_fired = True
+    elif keys[pygame.K_DOWN] and keys[pygame.K_LEFT] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "southwest"))
+        bullet_fired = True
+    elif keys[pygame.K_UP] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "up"))
+        bullet_fired = True
+    elif keys[pygame.K_DOWN] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "down"))
+        bullet_fired = True
+    elif keys[pygame.K_LEFT] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "left"))
+        bullet_fired = True
+    elif keys[pygame.K_RIGHT] and not bullet_fired:
+        player_bullets.append(PlayerBullet(player.x, player.y, "right"))
+        bullet_fired = True
+
+    if keys[pygame.K_w]:
+        player.y -= player.speed
+    if keys[pygame.K_a]:
+        player.x -= player.speed
+    if keys[pygame.K_s]:
+        player.y += player.speed
+    if keys[pygame.K_d]:
+        player.x += player.speed
+
+    display_scroll[0] = player.x - 400
+    display_scroll[1] = player.y - 300
+
     pygame.draw.rect(display, (255, 255, 255), (100-display_scroll[0], 100-display_scroll[1], 16, 16))
 
-    #if a key is pressed move screen to the right
-    if keys[pygame.K_a]:
-        display_scroll[0] -= 5
-    #if d key is pressed move screen to the left
-    if keys[pygame.K_d]:
-        display_scroll[0] += 5
-    #if w key is pressed move screen downward
-    if keys[pygame.K_w]:
-        display_scroll[1] -= 5
-    #if s key is pressed move screen upward
-    if keys[pygame.K_s]:
-        display_scroll[1] += 5
+    player.main(display, display_scroll)
 
-    #drawing the player on the display
-    player.main(display)
+    for bullet in player_bullets:
+        bullet.main(display, display_scroll)
 
-    #delay to get 60 fps
     clock.tick(60)
-
-    #updating the contents of the entire display
     pygame.display.update()
