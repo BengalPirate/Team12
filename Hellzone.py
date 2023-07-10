11a6719513c8eb13f7999f68bcb19500358655a6
Your Name <J00610524@students.jsums.edu>
2023-07-10 17:58:30 -0500

    EnemyAI

--- Hellzone.py
+++ Hellzone.py
@@ -2,20 +2,22 @@
 import pygame
 import sys 
 import math
+import random
 
 
 #Initializes all imported pygame modules
 pygame.init()
 
-pygame.mixer.init()
+all_sprites = pygame.sprite.Group()
+#pygame.mixer.init()
 
 #load mp3 file
-pygame.mixer.music.load('/home/x/Desktop/team12/Team12/final_boss.mp3')
+#pygame.mixer.music.load('Team12/final_boss.mp3')
 
 #Play the music indefinitely
-pygame.mixer.music.play(-1)
+#pygame.mixer.music.play(-1)
 
-print(pygame.get_error())
+#print(pygame.get_error())
 
 #sets th size of the screen for display
 display = pygame.display.set_mode((800, 600))
@@ -24,18 +26,20 @@ SCREEN_WIDTH, SCREEN_HEIGHT = display.get_size()
 # creates an object to help trakc time
 clock = pygame.time.Clock()
 
+scroll_offset = [0, 0]
+
 # creates a class for the player and attributes
 class Player:
     def __init__(self, x, y, width, height): # Define initital properties of the player
-        self.x = x
-        self.y = y
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
         self.max_health =100
         self.current_health = 100
         self.max_stamina = 100
         self.current_stamina = 100
         self.stamina_recovery_rate = 0.01
         self.max_power = 100
-        self.current_power = self.max_power
+        self.current_power = 100
         self.speed = 5 #define player's speed
         self.frame = 0
         self.moving_right = False
@@ -49,6 +53,10 @@ class Player:
         self.dash_speed = 10 # A faster speed for dashing
         self.dashing = False #Indicates whether the player is currently dashing
         
+#youtube tutorial of pygame
+#player_walk_images = [pygame.image.load("player_walk_0.png"), pygame.image.load("player_walk_0.png"),
+#pygame.image.load("player_walk_0.png"),pygame.image.load("player_walk_0.png"),]
+
 
         # checks files for images to use when player moves in a particular direction
         try:
@@ -66,16 +74,20 @@ class Player:
                 "southeast": [pygame.image.load('path_to_southeast_image_{}.png'.format(i)) for i in range(4)],
                 "southwest": [pygame.image.load('path_to_southwest_image_{}.png'.format(i)) for i in range(4)],
             }
-            self.current_image = self.images["right"]
-            self.width = self.current_image.get_width()
-            self.height = self.current_image.get_height()
+            self.current_image = self.images["right"][0]
+            self.image = pygame.Surface((width, height))
+            self.image.blit(self.current_image, (0,0))
             self.use_images = True
+            
+            #self.width = self.current_image.get_width()
+            #self.height = self.current_image.get_height()
+            #self.use_images = True
 
         # if no images can be found we default to a black triangle
         except (pygame.error, FileNotFoundError):
+            self.image = pygame.Surface((width, height))
+            self.image.fill((0, 0, 0))
             self.use_images = False
-            self.width = 32
-            self.height = 32
 
     #function to use in main program that updates the players image based on directional movement
     def update_image(self, direction):
@@ -126,9 +138,9 @@ class Player:
         
         if self.use_images:
             #the transform.scale changes the size of the player
-            display.blit(pygame.transform.scale(self.current_image, (32,42)), (self.x - scroll[0], self.y - scroll[1]))
+            display.blit(pygame.transform.scale(self.current_image, (32,42)), (self.rect.x - scroll[0], self.rect.y - scroll[1]))
         else:
-            pygame.draw.rect(display, (0,0,0), (self.x - scroll[0], self.y - scroll[1], self.width, self.height))
+            pygame.draw.rect(display, (0,0,0), (self.rect.x - scroll[0], self.rect.y - scroll[1], self.rect.width, self.rect.height))
   
         self.moving_right = False
         self.moving_left = False
@@ -179,9 +191,54 @@ class PlayerBullet:
 
         pygame.draw.circle(display, (0,0,0), (self.x - scroll[0], self.y - scroll[1]), 5)
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.animation_images = []
+        for i in range(4):
+            try:
+                self.animation_images.append(pygame.Surface((20,20)))
+            except pygame.error:
+                self.animation_images.append(pygame.Surface((20,20)))
+                self.animation_images[i].fill((0, 255, 0)) # Fills the enemy's surface with green color
+        self.animation_count = 0
+        self.reset_offset = 0
+        self.offset_x = random.randrange(-150, 150)
+        self.offset_y = random.randrange(-150, 150)
+    def main(self, display):
+        if self.animation_count +1 == 16:
+            self.animation_count = 0
+        self.animation_count += 1
 
+        if self.reset_offset == 0:
+            self.offset_x = random.randrange(-150, 150)
+            self.offset_y = random.randrange(-150, 150)
+            self.reset_offset = random.randrange(120, 150)
+        else: 
+            self.reset_offset -= 1
+
+        if player.rect.x + self.offset_x > self.x-display_scroll[0]:
+            self.x += 1
+        elif player.rect.x + self.offset_x < self.x-display_scroll[0]:
+            self.x -= 1
+
+        if player.rect.y + self.offset_y > self.y-display_scroll[1]:
+            self.y += 1
+        elif player.rect.y + self.offset_y < self.y-display_scroll[1]:
+            self.y -= 1
+        
+        display.blit(pygame.transform.scale(self.animation_images[self.animation_count//4],(32, 30)), (self.x-display_scroll[0], self.y-display_scroll[1]))
+
+        
+   
+        
 # Creates an instance of the player
-player = Player(400, 300, 32, 32) 
+player = Player(400, 300, 32, 32)
+
+enemies = [Enemy(200,300)]
+
 
 # Defines the initial display scroll
 display_scroll = [0,0]
@@ -198,7 +255,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # If the event is a quit, exit the game
             sys.exit()
-
         # Let's assume that pygame.K_SPACE is the key for starting a dash
 
 
@@ -209,136 +265,59 @@ while True:
     player.main(display, display_scroll)
     player.draw_bars(display)
 
+    all_sprites.update()
+    all_sprites.draw(display)
 
     #Check if bullet has been fired
     bullet_fired = False
+    
+    key_combinations = {
+        (pygame.K_UP, pygame.K_RIGHT): "northeast",
+        (pygame.K_UP, pygame.K_LEFT): "northwest",
+        (pygame.K_DOWN, pygame.K_RIGHT): "southeast",
+        (pygame.K_DOWN, pygame.K_LEFT): "southwest",
+        (pygame.K_UP,): "up",
+        (pygame.K_DOWN,): "down",
+        (pygame.K_LEFT,): "left",
+        (pygame.K_RIGHT,): "right"
+    }
 
-    # Check for key presses to determine buller direction, create bullet if conditions are met
-    if keys[pygame.K_UP] and keys[pygame.K_RIGHT] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "northeast"))
-        bullet_fired = True
-    elif keys[pygame.K_UP] and keys[pygame.K_LEFT] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "northwest"))
-        bullet_fired = True
-    elif keys[pygame.K_DOWN] and keys[pygame.K_RIGHT] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "southeast"))
-        bullet_fired = True
-    elif keys[pygame.K_DOWN] and keys[pygame.K_LEFT] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "southwest"))
-        bullet_fired = True
-    elif keys[pygame.K_UP] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "up"))
-        bullet_fired = True
-    elif keys[pygame.K_DOWN] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "down"))
-        bullet_fired = True
-    elif keys[pygame.K_LEFT] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "left"))
-        bullet_fired = True
-    elif keys[pygame.K_RIGHT] and not bullet_fired:
-        player_bullets.append(PlayerBullet(player.x, player.y, "right"))
-        bullet_fired = True
+    for combination, direction in key_combinations.items():
+        if all(keys[key] for key in combination) and not bullet_fired:
+            player_bullets.append(PlayerBullet(player.rect.x, player.rect.y, direction))
+            bullet_fired = True
+     
+    key_movements = {
+        (pygame.K_w,): ("up", 0, -1),
+        (pygame.K_a,): ("left", -1, 0),
+        (pygame.K_s,): ("down", 0, 1),
+        (pygame.K_d,): ("right", 1, 0),
+        (pygame.K_w, pygame.K_d): ("northeast", 1, -1),
+        (pygame.K_w, pygame.K_a): ("northwest", -1, -1),
+        (pygame.K_s, pygame.K_d): ("southeast", 1, 1),
+        (pygame.K_s, pygame.K_a): ("southwest", -1, 1)
+    }
 
-    # Move the player according to key presses and updates images
-    if keys[pygame.K_w]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.y -= player.dash_speed
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.y -= player.speed 
-        #player.y -= player.speed
-        player.update_image("up")
-        player.moving_up = True
-    if keys[pygame.K_a]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.x -= player.dash_speed
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.x -= player.speed 
-        #player.x -= player.speed
-        player.update_image("left")
-        player.moving_left = True
-    if keys[pygame.K_s]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.y += player.dash_speed
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.y += player.speed 
-        #player.y += player.speed
-        player.update_image("down")
-        player.moving_down = True
-    if keys[pygame.K_d]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.x += player.dash_speed
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.x += player.speed 
-        #player.x += player.speed
-        player.update_image("right")
-        player.moving_right = True
-    if keys[pygame.K_w] and keys[pygame.K_d]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.x += player.dash_speed / math.sqrt(2)
-            player.y -= player.dash_speed / math.sqrt(2)
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.x += player.speed / math.sqrt(2)
-            player.y -= player.speed / math.sqrt(2)
-        #player.x += player.speed
-        #player.y -= player.speed
-        player.update_image("northeast")
-        player.moving_northeast = True
-    if keys[pygame.K_w] and keys[pygame.K_a]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.x -= player.dash_speed / math.sqrt(2)
-            player.y -= player.dash_speed / math.sqrt(2)
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.x -= player.speed / math.sqrt(2)
-            player.y -= player.speed / math.sqrt(2)
-        #player.x -= player.speed
-        #player.y -= player.speed
-        player.update_image("northwest")
-        player.moving_northwest = True
-    if keys[pygame.K_s] and keys[pygame.K_d]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.x += player.dash_speed / math.sqrt(2)
-            player.y += player.dash_speed / math.sqrt(2)
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.x += player.speed / math.sqrt(2)
-            player.y += player.speed / math.sqrt(2)
-        #player.x += player.speed
-        #player.y += player.speed
-        player.update_image("southeast")
-        player.moving_southeast = True
-    if keys[pygame.K_s] and keys[pygame.K_a]:
-        if keys[pygame.K_SPACE] and (player.current_stamina > 0):
-            player.x -= player.dash_speed / math.sqrt(2)
-            player.y += player.dash_speed / math.sqrt(2)
-            player.current_stamina -= 0.5
-            pygame.time.set_timer(pygame.USEREVENT, 1000) #Dash lasts for 1 second
-        else:
-            player.x -= player.speed / math.sqrt(2)
-            player.y += player.speed / math.sqrt(2)
-        #player.x -= player.speed
-        #player.y += player.speed
-        player.update_image("southwest")
-        player.moving_southwest = True
+    for keys_combination, (direction, dx, dy) in key_movements.items():
+        if all(keys[key] for key in keys_combination):
+            if keys[pygame.K_SPACE] and (player.current_stamina > 0):
+                player.rect.x += player.dash_speed * dx / math.sqrt(2)
+                player.rect.y += player.dash_speed * dy / math.sqrt(2)
+                player.current_stamina -= 0.5
+                pygame.time.set_timer(pygame.USEREVENT, 1000)  # Dash lasts for 1 second
+            else:
+                player.rect.x += player.speed * dx / math.sqrt(2)
+                player.rect.y += player.speed * dy / math.sqrt(2)
+            player.update_image(direction)
+            setattr(player, "moving_" + direction, True)
+
 
     if player.dashing and pygame.time.get_ticks() / 1000 > player.dash_time + player.dash_duration:
         player.dashing = False    
 
     # Update the display scroll based on player's position
-    display_scroll[0] = player.x - 400
-    display_scroll[1] = player.y - 300
+    display_scroll[0] = player.rect.x - 400
+    display_scroll[1] = player.rect.y - 300
 
     # Draw a static rectangle to the screen, offset by the display scroll
     pygame.draw.rect(display, (255, 255, 255), (100-display_scroll[0], 100-display_scroll[1], 16, 16))
@@ -349,6 +328,9 @@ while True:
     # Update the bullet displays
     for bullet in player_bullets:
         bullet.main(display, display_scroll)
+
+    for Enemy in enemies:
+        Enemy.main(display)
 
     # Limit the game to 60 frames per second
     clock.tick(60)
